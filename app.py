ba96a866496fe4f138cbeb03adf2f90cb874e62e
Morimoto Shintaro <mshintaro014@gmail.com>
2023-10-16 14:04:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,65 @@
 from flask import Flask, render_template, request, jsonify
-import datetime 
+import os
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 
-TEXT_FILE = 'text.txt'
-LOG_FILE = 'log.json'
+# メモの内容を保存するファイルパス
+text_file = 'text.txt'
+# ログを保存するファイルパス
+log_file = 'log.txt'
 
-prev_text = ''
-
-def load_text():
-    with open(TEXT_FILE, 'r') as f:
-        return f.read()
-
-def save_text(text):
-    with open(TEXT_FILE, 'w') as f:
-        f.write(text)
-
-def write_log(text):
-    global prev_text
-
-    diff = abs(len(text) - len(prev_text))
-    if diff >= 30:
-        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        log = {'time': now, 'text': text}
-        with open(LOG_FILE, 'r+') as f:
-            logs = json.load(f) 
-            logs.append(log)
-            f.seek(0) 
-            json.dump(logs, f)
-
-    prev_text = text
+# ダークテーマのCSS
+dark_theme_css = """
+<style>
+    body {
+        background-color: #333;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+    }
+    textarea {
+        background-color: #444;
+        color: #fff;
+        width: 80%; /* ページの幅の80%を占める */
+        height: 300px; /* 適切な高さを設定 */
+        font-size:1.1rem;
+        padding:30px;
+    }
+</style>
+"""
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  # ページを開いたとき、text.txtの内容を読み込んでテキストエリアに表示
+  if os.path.exists(text_file):
+    with open(text_file, 'r') as file:
+      text = file.read()
+  else:
+    text = ''
 
-@app.route('/update_text', methods=['POST'])
+  return render_template('index.html', text=text, css=dark_theme_css)
+
+
+@app.route('/update', methods=['POST'])
 def update_text():
-    data = request.get_json()
-    text = data['text']
+  text = request.form.get('text', '')
 
-    save_text(text)
-    write_log(text)
+  # テキストをリアルタイムにtext.txtに保存
+  with open(text_file, 'w') as file:
+    file.write(text)
 
-    return jsonify({'status': 'ok'})
+  # ログを log.txt に追記
+  with open(log_file, 'a') as file:
+    file.write(text + '\n')
+
+  return jsonify({'status': 'success'})
+
 
 if __name__ == '__main__':
-    app.run()
+  app.run()
